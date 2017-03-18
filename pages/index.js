50f0a0f1fa04cb0b3e0919e57bb50f666a17cec9
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-03-18 03:44:21 -0300

    Refactoring pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,9 @@
-import React, {Component} from 'react'
-//import Router from 'next/router'
+import React, { PureComponent } from 'react'
 
-import Box from '../components/Box';
-import Buttons from '../components/Buttons';
-import Header from '../components/Header';
-import Logo from '../components/Logo';
+import Home from '../components/Home';
 
-import Globals from '../Globals';
-
-export default class extends Component {
+export default class extends PureComponent {
   render() {
-    return (
-      <div className='cont'>
-        <Header />
-
-        <div className='logo'>
-          <Logo />
-          <Buttons className='btns' />
-        </div>
-
-        <div className='footer'>
-          <Box>
-          <h3>DATA</h3>
-          <h2>{Globals.date.full}</h2>
-          <p>10:00am - 17:00pm</p>
-          </Box>
-          <Box>
-          <h3>LOCAL</h3>
-          <h2>{Globals.location.city}</h2>
-          <p>{Globals.location.locale}</p>
-          </Box>
-        </div>
-        <style jsx>{`
-          .cont {
-            background: #161616 url(/static/media/images/pattern.png);
-            color: white;
-            height: 100vh;
-            width: 100%;
-            min-width: 100vw;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            justify-content: space-between;
-          }
-
-          .logo {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-          }
-
-          .footer {
-            align-self: flex-end;
-            display: flex;
-            @media(max-width: 720px) {
-              align-self: auto;
-            }
-          }
-
-          .btns {
-            align-self: flex-end;
-            display: flex;
-            @media(max-width: 720px) {
-              align-self: auto;
-            }
-          }
-        `}</style>
-      </div>
-    )
+    return <Home />
   }
 }
